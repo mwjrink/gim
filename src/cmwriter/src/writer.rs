31c073783d6ae2a0f5eb0f2ebbd6350929909844
max <29183162+mwjrink@users.noreply.github.com>
2025-06-09 21:50:39 -0700

    Save State

--- src/cmwriter/src/writer.rs
+++ src/cmwriter/src/writer.rs
@@ -400,38 +400,68 @@ fn split<'a>(
     };
 
     {
-        let mut to_check = VecDeque::new();
-        to_check.push_back(idx_list[0]);
-        let mut hash_set = HashMap::with_capacity(idx_list.len());
+        let mut src_to_split_idx = HashMap::with_capacity(idx_list.len());
         for (idx, value) in idx_list.iter().enumerate() {
-            hash_set.insert(*value, idx);
+            src_to_split_idx.insert(*value, idx);
         }
+        let mut edge_0 = HashMap::new();
         // NEED TO ENSURE THE SPLITS ARE MANIFOLDS/CONTIGUOUS
         // TODO this breaks for non manifold tri pairs...
         // start from the lowest and go through, make sure everything is attached to it in some way
         // let mut settered = 0;
+        let mut to_check = VecDeque::new();
+        to_check.push_back(idx_list[0]);
+        let mut consumed = 0;
+        let mut last_resort = Vec::new();
         loop {
+            if consumed == split_size {
+                break;
+            }
+
             if let Some(check_idx) = to_check.pop_back() {
                 unsafe {
                     if source[check_idx].as_ref_unchecked().taken_by != idxs.0 {
                         source[check_idx].as_mut_unchecked().taken_by = idxs.0;
-                        // settered += 1;
+                        consumed += 1;
                     } else {
                         continue;
                     }
 
                     let tri = source[check_idx].as_ref_unchecked();
                     // connections is relative to the SOURCE SOURCE not the specific algo mesh...
-                    for connection in &tri.connections {
-                        // println!("Connection: {}", connection);
-                        if let Some(idx) = hash_set.get(connection) {
-                            // println!("Internal: {}", idx);
-                            if *idx < split_size
-                                && source[*connection].as_ref_unchecked().taken_by != idxs.0
-                            {
-                                // println!("Pushing: {}", connection);
-                                to_check.push_back(*connection);
+                    for (conn_idx, connection) in tri.connections.iter().enumerate() {
+                        if let Some(idx) = src_to_split_idx.get(connection) {
+                            let connected_tri = source[*connection].as_ref_unchecked();
+                            if connected_tri.taken_by != idxs.0 {
+                                if *idx < split_size {
+                                    to_check.push_back(*connection);
+                                } else {
+                                    last_resort.push(*idx);
+                                }
                             }
+
+                            // else {
+                            //     // match conn_idx {
+                            //     //     0 => {
+                            //     //         let i0 = indices[tri.idx * 3 + 0];
+                            //     //         let i1 = indices[tri.idx * 3 + 1];
+                            //     //         edge_0.insert([i0, i1], check_idx);
+                            //     //     }
+                            //     //     1 => {
+                            //     //         let i1 = indices[tri.idx * 3 + 1];
+                            //     //         let i2 = indices[tri.idx * 3 + 2];
+                            //     //         edge_0.insert([i1, i2], check_idx);
+                            //     //     }
+                            //     //     2 => {
+                            //     //         let i2 = indices[tri.idx * 3 + 2];
+                            //     //         let i0 = indices[tri.idx * 3 + 0];
+                            //     //         edge_0.insert([i2, i0], check_idx);
+                            //     //     }
+                            //     //     _ => {
+                            //     //         panic!("conn_idx is not 0, 1 or 2. This is compiler guaranteed. Either your PC is fucked, or a solar flare just flipped a bit.");
+                            //     //     }
+                            //     // }
+                            // }
                         }
                     }
                 }
@@ -440,31 +470,64 @@ fn split<'a>(
             }
         }
 
+        // TODO make sure there are asserts but non manifold should be impossble based on how we chose them.
         // assert_eq!(settered, split_size);
 
-        let mut non_manifold0 = Vec::new();
-        let mut non_manifold1 = Vec::new();
+        // let mut non_manifold0 = Vec::new();
+        // let mut non_manifold1 = Vec::new();
 
-        for (idx, tri_idx) in idx_list.iter().enumerate() {
-            let tri = unsafe { &source[*tri_idx].as_ref_unchecked() };
-            if idx < split_size {
-                if tri.taken_by != idxs.0 {
-                    non_manifold0.push(idx);
-                }
-            } else {
-                if tri.taken_by == idxs.0 {
-                    non_manifold1.push(idx);
-                }
-            }
-        }
+        // for (idx, tri_idx) in idx_list.iter().enumerate() {
+        //     let tri = unsafe { &source[*tri_idx].as_ref_unchecked() };
+        //     if idx < split_size {
+        //         if tri.taken_by != idxs.0 {
+        //             non_manifold0.push(idx);
+        //         }
+        //     } else {
+        //         if tri.taken_by == idxs.0 {
+        //             non_manifold1.push(idx);
+        //         }
+        //     }
+        // }
 
-        if non_manifold0.len() > 0 || non_manifold1.len() > 0 {
-            println!(
-                "Non Manifold Tris: {} vs {}",
-                non_manifold0.len(),
-                non_manifold1.len()
-            );
-        }
+        // loop {
+        //     if non_manifold0.len() > 0 && non_manifold1.len() > 0 {
+        //         let tri0_split_idx = non_manifold0.pop().unwrap();
+        //         let tri1_split_idx = non_manifold1.pop().unwrap();
+
+        //         idx_list.swap(tri0_split_idx, tri1_split_idx);
+        //         unsafe {
+        //             source[idx_list[tri1_split_idx]].as_mut_unchecked().taken_by = idxs.1;
+        //             source[idx_list[tri0_split_idx]].as_mut_unchecked().taken_by = idxs.0;
+        //         };
+        //     // } else if non_manifold0.len() > 0 {
+        //     //     let tri0_split_idx = non_manifold0.pop().unwrap();
+        //     //     let tri1_split_idx = dskfjsld;
+
+        //     //     idx_list.swap(tri0_split_idx, tri1_split_idx);
+        //     //     // find an edge tri in cluster 0
+        //     //     // swap it with the non manifold here.
+        //     // } else if non_manifold1.len() > 0 {
+        //     //     let tri1_split_idx = non_manifold1.pop().unwrap();
+        //     //     let tri0_split_idx = dskfjsld;
+
+        //     //     idx_list.swap(tri0_split_idx, tri1_split_idx);
+        //     //     // find an edge tri in cluster 1
+        //     //     // swap it with the non manifold here.
+        //     } else {
+        //         break;
+        //     }
+        // }
+
+        // assert_ne!(non_manifold0.len(), split_size);
+        // assert_ne!(non_manifold1.len(), split_size);
+
+        // if non_manifold0.len() > 0 || non_manifold1.len() > 0 {
+        //     println!(
+        //         "Non Manifold Tris: {} vs {}",
+        //         non_manifold0.len(),
+        //         non_manifold1.len()
+        //     );
+        // }
         // TODO finish non_manifold shit here
     };
 
